681552447f1005c77b4e96a0ef87c619912cc542
alecellis1985 <alecellis1985@gmail.com>
2016-07-13 15:43:55 -0300

    fsgds

--- resources/widgets/home.controller.js
+++ resources/widgets/home.controller.js
@@ -16,5 +16,16 @@
                 paragraph: 'Im a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.'
             }
         ];
+        vm.clients = [{
+                src: '../images/image.png',
+                alt: 'Architecture'
+            },
+            {
+                src: '../images/img.png',
+                alt: 'Architecture'
+            }, {
+                src: '../images/img3.png',
+                alt: 'Architecture'
+            }];
     }
 })();
